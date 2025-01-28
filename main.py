c09c20197fc810d200939395b3acc7da0112c677
Jan Fincke <jan.fincke@iki.fi>
2025-01-28 22:41:46 +0200

    Integrate Player, Asteroid, and AsteroidField classes into main game loop for entity management

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
@@ -8,14 +11,32 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         screen.fill("black")
-
+        
+        for entity in updatable:
+            entity.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
