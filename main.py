b55f8f8be20b0e385b035171fcc73d07055043fb
Jan Fincke <jan.fincke@iki.fi>
2025-01-28 21:59:54 +0200

    Initialize Pygame and set up the display with screen dimensions

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
+    while True:
+        screen.fill("black")
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+        pygame.display.flip()
 if __name__ == "__main__":
     main()
